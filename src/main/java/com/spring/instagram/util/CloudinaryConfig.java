c47a495f16a7b03b986a88af74d4c10b5f41bccb
luizgustv <luiz_gustavo_28@hotmail.com>
2020-08-28 14:46:44 -0300

    redefinindo variavel cloudinary

--- src/main/java/com/spring/instagram/util/CloudinaryConfig.java
+++ src/main/java/com/spring/instagram/util/CloudinaryConfig.java
@@ -4,19 +4,22 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import com.cloudinary.Cloudinary;
 
-@Component
+@Service
 public class CloudinaryConfig {
 	
 	public Cloudinary getConfig() {
 		
-		Map config = new HashMap();
-		config.put("cloud_name", "dildbjrev");
+		/*Map config = new HashMap();
+		config.put("cloud_name","dildbjrev");
 		config.put("api_key", "884276542269511");
 		config.put("api_secret", "opdq5gTON4zoZXe7Pd0mWllzmSU");
 		Cloudinary cloudinary = new Cloudinary(config);
+		*/
+		Cloudinary cloudinary = new Cloudinary("cloudinary://884276542269511:opdq5gTON4zoZXe7Pd0mWllzmSU@dildbjrev");
 		
 		return cloudinary;
 	}
